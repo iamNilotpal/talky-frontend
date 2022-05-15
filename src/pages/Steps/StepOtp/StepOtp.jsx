aa246275d9fadc7f90609d7e444031d13bfb59f8
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-05-15 21:12:21 +0530

    Add method for verifying otp and setting global user and auth state

--- src/pages/Steps/StepOtp/StepOtp.jsx
+++ src/pages/Steps/StepOtp/StepOtp.jsx
@@ -3,9 +3,26 @@ import Button from '../../../components/shared/Button/Button';
 import Card from '../../../components/shared/Card/Card';
 import TextInput from '../../../components/shared/TextInput/TextInput';
 import styles from './StepOtp.module.css';
+import { verifyOtp } from '../../../api/otp-service';
+import { errorToast } from '../../../utils';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectOtpData, setAuth } from '../../../store/authSlice';
 
-const StepOtp = ({ onNextClick }) => {
+const StepOtp = () => {
   const [otp, setOtp] = useState('');
+  const { hash, phone } = useSelector(selectOtpData);
+  const dispatch = useDispatch();
+
+  const handleOtpVerification = async () => {
+    try {
+      if (!otp) return errorToast('Enter 4-digit code sent your phone');
+      const { data } = await verifyOtp(phone, otp, hash);
+      console.log(data);
+      dispatch(setAuth(data.user));
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  };
 
   return (
     <div className={styles.cardWrapper}>
@@ -18,7 +35,12 @@ const StepOtp = ({ onNextClick }) => {
             setOtp((otp) => (e.target.value.length < 5 ? e.target.value : otp))
           }
         />
-        <Button text="Verify OTP" icon="arrow" iconAlt="Arrow icon" />
+        <Button
+          text="Verify OTP"
+          icon="arrow"
+          iconAlt="Arrow icon"
+          onClick={handleOtpVerification}
+        />
         <p className={styles.paragraph}>
           By entering your number, you’re agreeing to our Terms of Service and
           Privacy Policy. Thanks!
