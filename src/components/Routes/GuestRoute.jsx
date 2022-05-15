af2accb40fee1022686a395bf9309076bdec0669
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-05-15 21:12:52 +0530

    Subscribe to auth and user state

--- src/components/Routes/GuestRoute.jsx
+++ src/components/Routes/GuestRoute.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { Redirect } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectAuth } from '../../store/authSlice';
 
 const GuestRoute = ({ children, ...rest }) => {
-  const isAuth = false;
+  const isAuthed = useSelector(selectAuth);
+  const { state } = useLocation();
 
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        isAuth ? (
-          <Redirect to={{ pathname: '/rooms', state: { from: location } }} />
+        isAuthed ? (
+          <Redirect
+            to={{
+              pathname: state?.from || '/rooms',
+              state: { from: location },
+            }}
+          />
         ) : (
           children
         )
--- src/components/Routes/SemiProtectedRoute.jsx
+++ src/components/Routes/SemiProtectedRoute.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import { selectAuth, selectUser } from '../../store/authSlice';
 
 const SemiProtectedRoute = ({ children, ...rest }) => {
-  const isAuth = true;
-  const user = { name: 'Nilotpal', activated: false };
+  const isAuthed = useSelector(selectAuth);
+  const user = useSelector(selectUser);
 
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        !isAuth ? (
+        !isAuthed ? (
           <Redirect to={{ pathname: '/', state: { from: location } }} />
-        ) : isAuth && !user.activated ? (
+        ) : isAuthed && !user.activated ? (
           children
         ) : (
           <Redirect to={{ pathname: '/rooms', state: { from: location } }} />
