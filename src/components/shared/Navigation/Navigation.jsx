a6664c95fdd25c9c0761d032578a7bf23638b344
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-05-17 00:29:16 +0530

    Add login button and it's functionality

--- src/components/shared/Navigation/Navigation.jsx
+++ src/components/shared/Navigation/Navigation.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { logout } from '../../../api/otp-service';
+import { setName } from '../../../store/activateSlice';
+import {
+  selectAuth,
+  selectUser,
+  setAuth,
+  setOtpData,
+} from '../../../store/authSlice';
+import { errorToast } from '../../../utils';
+import Button from '../Button/Button';
 import styles from './Navigation.module.css';
 
 const Navigation = () => {
+  const user = useSelector(selectUser);
+  const authed = useSelector(selectAuth);
+  const dispatch = useDispatch();
+
+  const handleLogout = async () => {
+    try {
+      const { data } = await logout();
+      errorToast('Logged out :)');
+      dispatch(setAuth(data.user));
+      dispatch(setOtpData({ phone: '', hash: '' }));
+      dispatch(setName(''));
+    } catch ({ response }) {
+      errorToast(response.data.message);
+      dispatch(setAuth(response.data.user));
+    }
+  };
+
   return (
     <nav className={`conatiner ${styles.navbar}`}>
       <Link to="/" className={styles.logo}>
@@ -13,6 +41,9 @@ const Navigation = () => {
         />
         <span>Talky</span>
       </Link>
+      {authed && user.activated && (
+        <Button onClick={handleLogout} text="Logout" icon="shock" />
+      )}
     </nav>
   );
 };
