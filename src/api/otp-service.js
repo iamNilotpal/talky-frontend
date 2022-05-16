682e866b47a50ee4db2d5b60a17f9268a0a2e1ca
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-05-17 00:24:35 +0530

    Add response interceptor for refreshing tokens if expired

--- src/api/otp-service.js
+++ src/api/otp-service.js
@@ -1,6 +1,34 @@
+import axios from 'axios';
+
 import { api } from './index';
 
 export const sendOtp = (phone) => api.post('/send-otp', { phone });
 export const verifyOtp = (phone, otp, hash) =>
   api.post('/verify-otp', { phone, otp, hash });
 export const activateUser = (data) => api.post('/activate', data);
+export const logout = () => api.post('/logout');
+
+/* Refreshing Tokens in Background */
+api.interceptors.response.use(
+  (config) => config,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === 401 &&
+      originalRequest &&
+      !originalRequest._isRetry
+    ) {
+      originalRequest.isRetry = true;
+      try {
+        await axios.get(`${process.env.REACT_APP_API_URL}/refresh-tokens`, {
+          withCredentials: true,
+        });
+        return api.request(originalRequest);
+      } catch ({ response }) {
+        console.error(response);
+      }
+    }
+    throw error;
+  }
+);
