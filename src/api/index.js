b340ddef85e12dd2e79dd6f6382e1571f71bd7b5
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-05-16 16:40:54 +0530

    Add api route for activating user

--- src/api/index.js
+++ src/api/index.js
@@ -6,4 +6,5 @@ export const api = axios.create({
     'Content-Type': 'application/json',
     Accept: 'application/json',
   },
+  withCredentials: true,
 });
--- src/api/otp-service.js
+++ src/api/otp-service.js
@@ -3,3 +3,4 @@ import { api } from './index';
 export const sendOtp = (phone) => api.post('/send-otp', { phone });
 export const verifyOtp = (phone, otp, hash) =>
   api.post('/verify-otp', { phone, otp, hash });
+export const activateUser = (data) => api.post('/activate', data);
