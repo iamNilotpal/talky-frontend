434f2f8db136a898df290f65e48749d09e5f8997
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-05-16 16:42:04 +0530

    Add functions for verifying otp and resending otp and styles for otp page

--- src/pages/Steps/StepOtp/StepOtp.jsx
+++ src/pages/Steps/StepOtp/StepOtp.jsx
@@ -1,32 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../../../components/shared/Button/Button';
 import Card from '../../../components/shared/Card/Card';
 import TextInput from '../../../components/shared/TextInput/TextInput';
 import styles from './StepOtp.module.css';
-import { verifyOtp } from '../../../api/otp-service';
-import { errorToast } from '../../../utils';
+import { sendOtp, verifyOtp } from '../../../api/otp-service';
+import { errorToast, successToast } from '../../../utils';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectOtpData, setAuth } from '../../../store/authSlice';
+import { selectOtpData, setAuth, setOtpData } from '../../../store/authSlice';
 
 const StepOtp = () => {
   const [otp, setOtp] = useState('');
   const { hash, phone } = useSelector(selectOtpData);
   const dispatch = useDispatch();
 
+  useEffect(() => successToast('OTP sent to your phone number.'), []);
+
   const handleOtpVerification = async () => {
     try {
       if (!otp) return errorToast('Enter 4-digit code sent your phone');
       const { data } = await verifyOtp(phone, otp, hash);
       console.log(data);
-      dispatch(setAuth(data.user));
+      if (data.authed && data.ok) dispatch(setAuth(data.user));
     } catch (error) {
-      console.log(error.response.data.message);
+      errorToast(error.response.data.statusText);
     }
   };
 
+  const handleOtpSend = async () => {
+    const { data } = await sendOtp(phone);
+    successToast('OTP sent to your number.');
+    console.log(data);
+    dispatch(setOtpData({ phone: data.phone, hash: data.hash }));
+  };
+
   return (
-    <div className={styles.cardWrapper}>
-      <Card heading="Enter The OTP" icon="lock" iconAlt="Lock icon">
+    <div className={`${styles.cardWrapper} margin_top--big`}>
+      <Card heading="Enter 4-Digit Code" icon="lock" iconAlt="Lock icon">
         <TextInput
           type="tel"
           placeholder="Enter the code we just texted you"
@@ -35,12 +44,13 @@ const StepOtp = () => {
             setOtp((otp) => (e.target.value.length < 5 ? e.target.value : otp))
           }
         />
-        <Button
-          text="Verify OTP"
-          icon="arrow"
-          iconAlt="Arrow icon"
-          onClick={handleOtpVerification}
-        />
+        <div className={styles.otpWrapper}>
+          <Button text="Verify OTP" onClick={handleOtpVerification} />
+          <span onClick={handleOtpSend} id={styles.resend}>
+            Didn't receive? Resend Otp
+          </span>
+        </div>
+
         <p className={styles.paragraph}>
           By entering your number, you’re agreeing to our Terms of Service and
           Privacy Policy. Thanks!
