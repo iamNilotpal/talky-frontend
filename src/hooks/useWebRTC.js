9ad49c7eb5e2042932d6a492030473fa2f1e2d5b
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2022-09-25 10:06:19 +0530

    Add custom hook for using WebRTC

---
+++ src/hooks/useWebRTC.js
@@ -0,0 +1,43 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { toastifyErrorMessage } from '../utils';
+import { useStateWithCallback } from './useStateWithCallback';
+
+export const useWebRTC = (roomId, user) => {
+  const [clients, setClients] = useStateWithCallback([]);
+  const audioElements = useRef({});
+  // const peerConnections = useRef({});
+  const localMediaStream = useRef(null);
+
+  const updateAudioRef = (instance, clientId) =>
+    (audioElements.current[clientId] = instance);
+
+  const addNewClient = useCallback(
+    (newClient, cb) => {
+      const doesExist = clients.find((c) => c.id === newClient.id);
+      if (!doesExist)
+        setClients((prevClients) => [...prevClients, newClient], cb);
+    },
+    [clients, setClients]
+  );
+
+  useEffect(() => {
+    (async function () {
+      try {
+        localMediaStream.current = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
+        addNewClient(user, () => {
+          const localElement = audioElements.current[user.id];
+          if (localElement) {
+            localElement.volume = 0;
+            localElement.srcObject = localMediaStream.current;
+          }
+        });
+      } catch (error) {
+        toastifyErrorMessage('Permission denied.');
+      }
+    })();
+  }, [user, addNewClient]);
+
+  return { clients, updateAudioRef };
+};
--- src/pages/Room/Room.jsx
+++ src/pages/Room/Room.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { useWebRTC } from '../../hooks/useWebRTC';
+import { selectUser } from '../../store/authSlice';
 
 const Room = () => {
   const { roomId } = useParams();
-  const { state } = useLocation();
+  const user = useSelector(selectUser);
+  const { clients, updateAudioRef } = useWebRTC(roomId, user);
+
   return (
-    <div className="container">
-      <h1>Room Id: {roomId}</h1>
-      <h1>{state && JSON.stringify(state.room, null, 4)}</h1>
+    <div className='container'>
+      {clients.map((client) => (
+        <p key={client.id}>
+          <span>{client.name}</span>
+          <audio
+            controls
+            ref={(instance) => updateAudioRef(instance, client.id)}
+            autoPlay
+          />
+        </p>
+      ))}
     </div>
   );
 };
